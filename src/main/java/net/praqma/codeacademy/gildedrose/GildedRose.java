214a42053d9bbbdeabf52ba2f4a0b09eb347d209
Daniel Vinther Wolf <danielvinther@youmail.dk>
2019-07-04 11:43:23 +0200

    GildedRose Update Quality, Sulfuras is always 80 and Aged Bries increments in quality (caps at 50) 2nd try

--- src/main/java/net/praqma/codeacademy/gildedrose/GildedRose.java
+++ src/main/java/net/praqma/codeacademy/gildedrose/GildedRose.java
@@ -19,14 +19,14 @@ class GildedRose {
             } else {
                 if (items[i].name.equals("Aged Brie")
                     if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1; 
-                    }   
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
+                        items[i].quality = items[i].quality + 1;
+                        }   
+                        if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                         if (items[i].sellIn < 11) {
+                              if (items[i].quality < 50) {
                                 items[i].quality = items[i].quality + 1;
-                            }
-                        }
+                                }
+                         }
 
                         if (items[i].sellIn < 6) {
                             if (items[i].quality < 50) {
